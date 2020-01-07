269dc91cbd17deb99ae3c7567fe4d4a463f798c7
Adrianna <addie.nosal@gmail.com>
2020-01-07 19:59:03 +0000

    add post pages, read more links

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -40,9 +41,11 @@ app.get("/posts/:title", (req, res) => {
   const requestedTitle = req.params.title;
   posts.forEach(post => {
     const storedTitle = post.title; //assign each title to storedTitle to compare it later
-
-    if (storedTitle === requestedTitle) {
-      console.log("Match found");
+    if (_.lowerCase(storedTitle) === _.lowerCase(requestedTitle)) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
     }
   });
 });
